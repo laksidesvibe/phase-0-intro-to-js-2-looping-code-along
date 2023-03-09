13ee2176496fc2ccad4545a48747f0ecbe0ceb65
lekan <laksidesvibe@gmail.com>
2023-03-08 23:40:22 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const names = ["Guadalupe","Ollie","Aki"];
+const occasion = "suprise";
+
+function writeCards(names,occasion) {
+  let greeting =[]
+  for (let i = 0; i < names.length; i++) {
+    greeting.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    
+  }
+
+  return greeting
+}
+
+function countDown (){
+    let counting = 10;
+    while (counting > -1) {
+      console.log(counting--);
+    }
+    
+}
+
